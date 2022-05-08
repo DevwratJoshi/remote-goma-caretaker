30a666d4a22f30c421712dc663561d0671f26cb0
Devwrat Joshi <devwrat.joshi1234@gmail.com>
2022-05-08 15:10:11 +0900

    Add toggling save and search resolution

--- scripts/test.py
+++ scripts/test.py
@@ -9,35 +9,50 @@ from numpy import array, zeros, isclose
 # Resolution used when checking for motion (low resolution for faster searching)
 def loadConfig(fpath):
     return json.load(open(fpath, 'r'))
+def GetToggledResolution(camera, config):
+    width = str(camera.resolution).split('x')[0]
+    height = str(camera.resolution).split('x')[1]
 
-config = loadConfig('../config/camera.conf.json')
+    if int(width) == config["searchResolution"]["width"] and  int(height) == config["searchResolution"]["height"]:
+        return config["saveResolution"]["width"], config["saveResolution"]["height"]
+        
+    return config["searchResolution"]["width"], config["searchResolution"]["height"]
+
+    
 
 
 # Resolution used to check for motion in the frame
-searchResolution = (config["searchResolution"]["width"], config["searchResolution"]["height"])
-saveResolution = (config["saveResolution"]["width"], config["saveResolution"]["height"])
 
-camera = PiCamera()
-camera.resolution = searchResolution
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=searchResolution)
-# allow the camera to warmup
-time.sleep(0.1)
-# capture frames from the camera
+def main():
+    config = loadConfig('../config/camera.conf.json')
+    searchResolution = (config["searchResolution"]["width"], config["searchResolution"]["height"])
+    saveResolution = (config["saveResolution"]["width"], config["saveResolution"]["height"])
+    camera = PiCamera()
+    camera.resolution = searchResolution
+    camera.framerate = 32
+    rawCapture = PiRGBArray(camera, size=searchResolution)
+    # allow the camera to warmup
+    time.sleep(0.1)
+    # capture frames from the camera
+    while True:
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+            # grab the raw NumPy array representing the image, then initialize the timestamp
+            # and occupied/unoccupied text
+            image = frame.array
+            # show the frame
+            cv2.imshow("Frame", image)
+            key = cv2.waitKey(1) & 0xFF
+            # clear the stream in preparation for the next frame
+            rawCapture.truncate(0)
+            # if the `q` key was pressed, break from the loop
+            if key == ord("q"):
+                return
+            elif key == ord("r"):
+                newResolution = GetToggledResolution(camera, config)
+                camera.resolution = newResolution
+                rawCapture = PiRGBArray(camera, size=newResolution)
+                time.sleep(0.1)
+                break
 
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # grab the raw NumPy array representing the image, then initialize the timestamp
-    # and occupied/unoccupied text
-    image = frame.array
-    # show the frame
-    cv2.imshow("Frame", image)
-    key = cv2.waitKey(1) & 0xFF
-    # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
-#    elif key == ord("r"):
-#        camera.resolution = saveResolution
-#        rawCapture = PiRGBArray(camera, size=saveResolution)
-#        time.sleep(0.1)
+if __name__ == "__main__":
+    main()
